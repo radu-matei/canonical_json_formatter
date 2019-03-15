11a6f06e40660f9bd5a08bea6373dcc2ed40bf49
Radu Matei <radu@radu-matei.com>
2019-03-15 21:53:35 +0200

    Simplify deserialization from slice in test

--- src/tests.rs
+++ src/tests.rs
@@ -1,7 +1,6 @@
 use crate::formatter;
 use serde::Serialize;
 use spectral::prelude::*;
-use std::str;
 
 #[test]
 fn test_can_use_formatter() {
@@ -10,7 +9,7 @@ fn test_can_use_formatter() {
     let mut ser = serde_json::Serializer::with_formatter(&mut out, formatter::Canonical {});
     obj.serialize(&mut ser).unwrap();
 
-    let x: serde_json::Value = serde_json::from_str(&str::from_utf8(&out).unwrap()).unwrap();
+    let x: serde_json::Value = serde_json::from_slice(&out).unwrap();
     assert_that(&x["foo"].as_bool().unwrap()).is_true();
     assert_that(&x["bar"].as_bool().unwrap()).is_false();
 }
